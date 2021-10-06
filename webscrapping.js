262d2e6fa674e97ee69b291276804886393c9f4d
Mayank Kumar <mk6205495206@gmail.com>
2021-10-06 17:22:34 +0530

    html scraped

---
+++ webscrapping.js
@@ -0,0 +1,25 @@
+//the purpose of this prroject is  to exract information from cricinfo 
+//and present in form of excel and scorecard
+//the real purpose is to learn howto  extract information
+//and get experience with javascript
+//a very good reason to ever make a project
+let minimist=require("minimist");
+let axios=require("axios");
+let jsdom=require("jsdom");
+let excel4node=require("excel4node");
+let pdf=require("pdf-lib");
+let args=minimist(process.argv);
+console.log(args);
+//download using axios
+// read using jsdom
+//make eexcel  using excel4node
+//make pdf using pdf-lib
+let response=axios.get(args.source);
+response.then(function (response) {
+       let html=response.data;
+       console.log(html);
+} )
+.catch(function (error) {
+  console.log(error);
+})
+
